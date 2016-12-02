870e3e82f98a541c2c575767bccc8cf18e69ef4e
jqsun <wukong@bi.a.u-tokyo.ac.jp>
2016-12-02 15:35:33 +0900

    changed from google api to plotly api.

--- PyDAIR/io/PyDAIRReport.py
+++ PyDAIR/io/PyDAIRReport.py
@@ -50,6 +50,12 @@ class PyDAIRReport:
         
         # set up basic statistics
         sample_stats = []
+        freq_stats = [{'name': 'V', 'freq': []},
+                       {'name': 'D', 'freq': []},
+                       {'name': 'J', 'freq': []}]
+        cdr3_stats = []
+        rare_stats = []
+        vdjidx = {'v': 0, 'd': 1, 'j': 2}
         for i in range(len(self.__stats.samples)):
             bsample = self.__stats.samples.get_record(i)
             sample_stats.append({'name': bsample.name,
@@ -61,35 +67,31 @@ class PyDAIRReport:
                                  'vdjrarefaction': self.__file_path['vdjrarefaction'][i],
                                  'cdr3protlen': self.__file_path['cdr3protlen'][i],
                                  'cdr3nucllen': self.__file_path['cdr3nucllen'][i]})
+            
+            for g in ['v', 'd', 'j']:
+                freq_g = bsample.get_freq(g, prob = True)
+                freq_stats[vdjidx[g]]['freq'].append({'sample_name': '"' + bsample.name + '"',
+                         'gene': '["' + '","'.join(map(str, freq_g.index.get_values())) + '"]',
+                         'freq': '[' + ','.join(map(str, freq_g.tolist())) + ']'})
+            
+            cdr3_g = bsample.get_freq('cdr3_prot_len', prob = True, remove_cdr3_zero_len = True)
+            cdr3_stats.append({'sample_name': '"' + bsample.name + '"',
+                                'length': '[' + ','.join(map(str, cdr3_g.index.get_values())) + ']',
+                                'freq': '[' + ','.join(map(str, cdr3_g.tolist())) +  ']'})
+            
+            if bsample.div.rarefaction['vdj'] is not None:
+                rare_stats.append({'sample_name': '"' + bsample.name + '"',
+                                   'vdj_combn': '[' + ','.join(map(str, bsample.div.rarefaction['vdj'].mean(axis = 1))) + ']',
+                                   'sample_size': '[' + ','.join(map(str, bsample.div.rarefaction['vdj'].index.get_values())) + ']'})
+            
         report_data['sample_stats'] = sample_stats
-        
-        
-        # set up V, D, and J statistics
-        freq_stats = []
-        for gene in ['v', 'd', 'j']:
-            freq_g = self.__stats.get_freq(gene)
-            freq_stats.append({'gene': gene.upper(),
-                               'count': min([freq_g.shape[0], 10]) + 2,
-                               'freq_csv' :  freq_g.to_csv(index_label = 'Gene', sep = '\t'),
-                               'freq_json':  freq_g.to_json()})
         report_data['freq_stats'] = freq_stats
-        
-        
-        # set up CDR3
-        cdr3_len_freq = self.__stats.get_cdr3len_freq()
-        cdr3_stats = {'freq_csv': cdr3_len_freq.to_csv(index_label = "Length", sep = '\t'),
-                      'freq_json': cdr3_len_freq.to_json()}
         report_data['cdr3_stats'] = cdr3_stats
+        report_data['rare_stats'] = rare_stats
         
         
-        # set up VDJ rarefaction data
-        rfdata = self.__stats.get_rarefaction_result(fun = 'mean')
-        if rfdata is not None:
-            report_data['rarefaction_stats'] = {'tries_csv': rfdata.to_csv(index_label = "CapturedSeq", sep = '\t'),
-                                                'tries_json': rfdata.to_json()}
-        else:
-            report_data['rarefaction_stats'] = None
-        
+
+
         
         
         html = self.__tmpl.render(report_data)
